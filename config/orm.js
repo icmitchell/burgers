6302a46558925cb9e3e6653189688c374a5d58a9
icmitchell <icmitchell73@yahoo.com>
2018-03-17 09:36:52 -0500

    final

---
+++ config/orm.js
@@ -0,0 +1,56 @@
+var connection = require("./connection.js");
+
+var orm = {
+
+  select: function(tableInput, callB) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      callB(result);
+    });
+  },
+
+  new: function(table, cols, name, callB) {
+    var values = {
+      burger_name: name,
+      devoured: false
+    }
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(values.length);
+    queryString += ") ";
+
+    connection.query(queryString, values, function(err, result) {
+      if (err) {
+        throw err; 
+      }
+      callB(result);
+    });
+  },
+
+  update: function(table, values, instance, callB) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(values);
+    queryString += " WHERE ";
+    queryString += instance;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      callB(result);
+    });
+  }
+};
+
+module.exports = orm;
+
